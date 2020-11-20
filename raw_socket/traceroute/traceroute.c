92f3c10958a2a914140437a9aabb6b3f81b35c41
synclling <1156721870@qq.com>
2020-11-20 23:25:32 +0800

    traceroute

--- raw_socket/traceroute/traceroute.c
+++ raw_socket/traceroute/traceroute.c
@@ -1,18 +1,19 @@
 #include "traceroute.h"
 
-uint16_t in_cksum(uint16_t *addr, int len, int csum)
+uint16_t icmp_cksum(uint16_t *data, int len)
 {
-	int sum = csum;
+	uint32_t sum = 0;
 
 	while(len > 1)
 	{
-		sum += *addr++;
+		sum += *data++;
 		len -= 2;
 	}
 
 	if(len == 1)
 	{
-		sum += htons(*(uint8_t)addr << 8);
+		uint8_t tmp = *(uint8_t *)data;
+		sum += tmp;
 	}
 
 	sum = (sum >> 16) + (sum & 0xffff);
@@ -20,6 +21,33 @@ uint16_t in_cksum(uint16_t *addr, int len, int csum)
 
 	return ~sum;
 }
+
+void icmp_pack(struct icmp *icmph, int seq)
+{
+	icmph->icmp_type = ICMP_ECHO;
+	icmph->icmp_code = 0;
+	icmph->icmp_cksum = 0;
+	icmph->icmp_id = pid & 0xffff;
+	icmph->icmp_seq = seq & 0xffff;
+
+	icmp->icmp_cksum = icmp_cksum((uint16_t *)icmph, ICMP_HEADER_LEN); // 此icmp报文没有数据，故只校验头部长度
+}
+
+double time_difference(struct timeval begin, struct timeval end)
+{
+	struct timeval tv;
+	tv.tv_sec = end.tv_sec - begin.tv_sec;
+	tv.tv_usec = end.tv_usec - begin.tv_usec;
+
+	if(tv.usec < 0)
+	{
+		tv.tv_sec--;
+		tv.tv_usec += 1000000;
+	}
+
+	return tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
+}
+
 
 
 int main(int argc, char *argv[])
@@ -30,10 +58,10 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	char dest_str[128];				// 目的主机字符串
+	char dest_str[128];			// 目的主机字符串
 	memcpy(dest_str, argv[1], strlen(argv[1]) + 1);
 
-	pid_t pid = getpid();			// 获取当前进程ID
+	pid = getpid();				// 获取当前进程ID
 
 	struct sockaddr_in addr;
 	bzero(&addr, sizeof(addr));
@@ -50,10 +78,46 @@ int main(int argc, char *argv[])
 	struct timeval tv;
 	tv.tv_sec = 0;
 	tv.tv_usec = 1000;
-	setsockopt(rawsock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+	setsockopt(rawsock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)); // set time limit of socket's waiting for a packet
+
+	int seq = 1;
+	int recvcnt = 0;
+	
+	struct timeval begin, current;
+	struct timeval sendtime[REQUEST_PER_TTL];
 	
 	for(int ttl = 1; ttl <= TTL_LIMIT; ++ttl)
 	{
-		
+		setsockopt(rawsock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+
+		for(int i = 0; i < REQUEST_PER_TTL; ++i)
+		{
+			icmp_pack(send_buf, seq++);
+
+			gettimeofday(&sendtime[i], NULL);
+			sendto(rawsock, sendbuf, ICMP_HEADER_LEN, 0, (struct sockaddr *)&addr, sizeof(addr));
+		}
+
+		gettimeofday(&begin, NULL); // get time after sending the packets
+
+		while(recvcnt < REQUEST_PER_TTL)
+		{
+			int res = recvfrom(rawsock, recvbuf, BUFFER_SIZE, 0, 0, 0);
+			if(res < 0)
+			{
+				gettimeofday(&current, NULL);
+				if(time_difference(begin, current) > TIMEOUT) break;
+				continue;
+			}
+
+			struct ip *iph = (struct ip *)recvbuf;
+			if(iph->ip_p != IPPROTO_ICMP)
+			{
+				continue;
+			}
+
+			struct icmp *icmph = (struct icmp *)(recvbuf + iph->ip_hl * 4);
+			
+		}
 	}
 }
--- raw_socket/traceroute/traceroute.h
+++ raw_socket/traceroute/traceroute.h
@@ -3,20 +3,26 @@
 
 #include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <sys/time.h>
 
 #include <arpa/inet.h>
 #include <netinet/ip_icmp.h>
 
 
-#define TTL_LIMIT		30
-#define REQUEST_PER_TTL	3
-#define BUFFER_SIZE 	1024
+#define TIMEOUT				1000	// miliseconds(1000 ms = 1 s)
+#define TTL_LIMIT			30
+#define REQUEST_PER_TTL		3
+#define BUFFER_SIZE 		1024
+#define ICMP_HEADER_LEN		8
 
-char send_buf[BUFFER_SIZE];
-char recv_buf[BUFFER_SIZE];
+char sendbuf[BUFFER_SIZE];
+char recvbuf[BUFFER_SIZE];
 
+static pid_t pid;
 
-uint16_t in_cksum(uint16_t *addr, int len, int csum);
+uint16_t icmp_cksum(uint16_t *data, int len);
 
+void icmp_pack(struct icmp *icmph, int seq);
 
+double time_difference(struct timeval begin, struct timeval end);
