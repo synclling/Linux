9637450788ab54c111d5a3111017b8547a2a65d3
synclling <1156721870@qq.com>
2021-01-31 22:02:05 +0800

    svmsgcliserv

--- IPC/svmsg/svmsgcliserv/server.c
+++ IPC/svmsg/svmsgcliserv/server.c
@@ -7,14 +7,14 @@ void server(int readfd, int writefd)
 	struct mymesg	mesg;
 	
 	mesg.mesg_type = 1;
-	if((n = mesg_recv(readfd, &mymesg)) == 0)
+	if((n = mesg_recv(readfd, &mesg)) == 0)
 		err_quit("pathname missing");
 	mesg.mesg_data[n] = '\0';	// null terminate pathname
 
 	if((fp = fopen(mesg.mesg_data, "r")) == NULL) {
 		snprintf(mesg.mesg_data + n, sizeof(mesg.mesg_data) - n, ": can't open, %s\n", strerror(errno));
 		mesg.mesg_len = strlen(mesg.mesg_data);
-		mesg_send(writefd, &mymesg);
+		mesg_send(writefd, &mesg);
 	} else {
 		/* for open succeeded: copy file to IPC channel */
 		while(fgets(mesg.mesg_data, MAXMESGDATA, fp) != NULL) {
--- IPC/svmsg/svmsgcliserv/svmsg.h
+++ IPC/svmsg/svmsgcliserv/svmsg.h
@@ -3,3 +3,5 @@
 #define MQ_KEY1		1234L
 #define MQ_KEY2		2345L
 
+#define MSG_R		0400
+#define MSG_W		0200
