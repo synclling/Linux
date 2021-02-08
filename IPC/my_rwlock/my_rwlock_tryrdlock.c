7e7fb188fc7dc73f3dbfd2918454ed4bf810a038
synclling <1156721870@qq.com>
2021-02-08 23:34:02 +0800

    my rwlock

--- IPC/my_rwlock/my_rwlock_tryrdlock.c
+++ IPC/my_rwlock/my_rwlock_tryrdlock.c
@@ -11,7 +11,7 @@ int my_rwlock_tryrdlock(my_rwlock_t *rw)
 	if((result = pthread_mutex_lock(&rw->rw_mutex)) != 0)
 		return (result);
 
-	if(rw->rw_refcount < 0 || rw->rw_nwaitwriters > 0)
+	if(rw->rw_refcount == -1 || rw->rw_nwaitwriters > 0)
 		result = EBUSY;
 	else
 		rw->rw_refcount++;
